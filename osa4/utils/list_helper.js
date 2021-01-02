380d810f2c9a67b74a39b81b8a15dc8dc5f193b9
morningstreams <joona00ramo@gmail.com>
2021-01-02 15:50:43 +0200

    4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -8,11 +8,19 @@ const totalLikes = (blogs) => {
   } else if (blogs.length === 1) {
     return blogs[0].likes;
   } else {
-    return blogs.reduce((acc, curr) => acc + curr.likes, 0);
+    return blogs.reduce((acc, cur) => acc + cur.likes, 0);
   }
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  return blogs.reduce((acc, cur) => (acc.likes > cur.likes ? acc : cur));
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
