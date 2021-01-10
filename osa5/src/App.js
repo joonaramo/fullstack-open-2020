cd4ac389fb163731f976467cede16f3c73c396e4
morningstreams <joona00ramo@gmail.com>
2021-01-10 22:47:01 +0200

    5.9

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -90,9 +90,11 @@ const App = () => {
           blogFormRef={blogFormRef}
         />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
     </div>
   );
 };
