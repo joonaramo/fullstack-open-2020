f82c9061c77bbe56060da25d7453e63e26d9e589
morningstreams <joona00ramo@gmail.com>
2021-01-16 18:12:44 +0200

    5.14

--- osa5/src/components/Blog.test.js
+++ osa5/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
 test('renders title and author, not url and likes by default', () => {
@@ -17,4 +17,21 @@ test('renders title and author, not url and likes by default', () => {
   expect(component.container).toHaveTextContent('John Doe');
   expect(component.container).not.toHaveTextContent('https://example.com/blog');
   expect(component.container).not.toHaveTextContent('likes 8');
+});
+
+test('clicking view button shows url and likes', async () => {
+  const blog = {
+    title: 'React Blog',
+    author: 'John Doe',
+    url: 'https://example.com/blog',
+    likes: 8,
+    user: {
+      username: 'Mary Poppins',
+    },
+  };
+  const component = render(<Blog blog={blog} />);
+  const button = component.getByText('view');
+  fireEvent.click(button);
+  expect(component.container).toHaveTextContent('https://example.com/blog');
+  expect(component.container).toHaveTextContent('likes 8');
 });
