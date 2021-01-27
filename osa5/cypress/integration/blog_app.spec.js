ef8e9abc88e994d017c84d50a1cf5e24b017e7d1
Joona Rämö <joona00ramo@gmail.com>
2021-01-27 15:49:55 +0200

    5.18

--- osa5/cypress/integration/blog_app.spec.js
+++ osa5/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {
+      name: 'John Doe',
+      username: 'jdoe',
+      password: 'secret',
+    };
+    cy.request('POST', 'http://localhost:3003/api/users', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -8,5 +14,21 @@ describe('Blog app', function () {
     cy.get('#username');
     cy.get('#password');
     cy.get('#login');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('jdoe');
+      cy.get('#password').type('secret');
+      cy.get('#login').click();
+      cy.contains('jdoe logged in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('wrong');
+      cy.get('#password').type('credentials');
+      cy.get('#login').click();
+      cy.contains('invalid username or password');
+    });
   });
 });
