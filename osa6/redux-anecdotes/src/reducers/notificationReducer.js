49e773a6d94a4cbf87cffc58fdde4727e1016c9a
joonaramo <joona00ramo@gmail.com>
2021-02-24 12:31:26 +0200

    push

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+const initialState = 'Error! Please try again!';
+
+const notificationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default notificationReducer;
