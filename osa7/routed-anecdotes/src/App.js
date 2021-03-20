78905751ee148f4275caa84d045d688007a492af
joonaramo <joona00ramo@gmail.com>
2021-03-20 15:19:44 +0200

    7.6

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -95,9 +95,9 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const history = useHistory();
-  const content = useField('text');
-  const author = useField('text');
-  const info = useField('text');
+  const { reset: resetContent, ...content } = useField('text');
+  const { reset: resetAuthor, ...author } = useField('text');
+  const { reset: resetInfo, ...info } = useField('text');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -112,9 +112,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault();
-    content.reset();
-    author.reset();
-    info.reset();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
